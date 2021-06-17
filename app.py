0004cc595caf6fd0be05d2d129870bfca31685c8
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-06-17 13:29:03 +0200

    funkar html

--- app.py
+++ app.py
@@ -1,12 +1,41 @@
 # Web applikation
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, IntegerField
+from wtforms.validators import DataRequired, Length
+
+def CalculateSalary(namn, timlon, hours):
+    if namn.lower() == "stefan":
+        timlon = timlon * 2
+    return hours * timlon
+
 
 app = Flask(__name__)
+# Flask-WTF requires an encryption key - the string can be anything
+app.config['SECRET_KEY'] = 'C2HWGVoMGfNTBsrYQg8EcMrdTimkZfAb'
+
+class SalaryForm(FlaskForm):
+    name = StringField("Vad heter du?",validators=[DataRequired(),Length(min=4,
+        message=('Minst 4 tecken tack ')
+    )])
+    hourlySalary = IntegerField('Timlön',  validators=[DataRequired()])
+    hoursWorked = IntegerField('Timmar du jobbat',  validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 
-@app.route("/salary")
+
+@app.route("/salary", methods=['GET', 'POST'])
 def salary():
-    return render_template("salary.html")
+    form = SalaryForm()
+    message = ""
+    if form.validate_on_submit():
+        name = form.name.data
+        hoursWorked = int(form.hoursWorked.data)
+        hourlySalary = int(form.hourlySalary.data)
+        salary = CalculateSalary(name, hourlySalary, hoursWorked)
+        message = f"Din lön är {salary}"
+    #return render_template('nameage.html', form=form, message=message)    
+    return render_template("salary.html", form=form, message=message)
 
 
 
@@ -17,6 +46,6 @@ def hej():
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World Stefan!</p>"
+    return "<p>Hej <a href='/salary'>Kör lönekalkylatorn här!</a></p>"
 
 app.run()    
