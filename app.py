5ab0c073b645e144f86dbba01c626a23c8e7d228
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-06-17 11:39:37 +0200

    salry

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 # Web applikation
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
+@app.route("/salary")
+def salary():
+    return render_template("salary.html")
+
+
+
+@app.route("/hej.html")
+def hej():
+    return "Hejsan <b>på dig</b>"
+
 
 @app.route("/")
 def hello_world():
