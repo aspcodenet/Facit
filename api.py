73b4181eccb25dfe1ecc448c361e5e7bfed9b600
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-06-18 10:51:35 +0200

    Lista alla

--- api.py
+++ api.py
@@ -14,8 +14,47 @@ class Bil(db.Model):
     namn = db.Column(db.String(80), unique=False, nullable=False)
     modell = db.Column(db.String(20), unique=False, nullable=False)
     year = db.Column(db.Integer, unique=False, nullable=False)
-    color = db.Column(db.String(10), unique=False, nullable=False)
 
-db.create_all()
+    def serialize(self):
+        return {
+            'modell': self.modell,
+            'namn': self.namn,
+            'year': self.year,
+            'id': self.id
+        }    
+
+
+
+@app.route('/api/bil', methods=['GET'])
+def listaAlla():
+    q = Bil.query.all()
+    return jsonify([b.serialize() for b in q])
+
+
+
+# REST API
+# /api/bil = GET  LISTA PÅ ALLA
+# /api/bil/1 = GET  SE EN BIL
+# /api/bil = POST  json som ny NY
+# /api/bil/1 = DELETE  delete
+# /api/bil/1 = PUT/PATCH  udate
+
+# b = Bil()
+# b.namn = "Volvo"
+# b.modell = "XC60"
+# b.year = 2016
+
+# b2 = Bil()
+# b2.namn = "Saab"
+# b2.modell = "V4"
+# b2.year = 1973
+
+
+# db.session.add(b)                       
+# db.session.add(b2)                       
+# db.session.commit()
+
+
+#db.create_all()
 
 app.run()
