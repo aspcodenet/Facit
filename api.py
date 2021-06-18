8cc8da8823b940672b3ce197a5b215585b749a95
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-06-18 10:32:30 +0200

    api2

--- api.py
+++ api.py
@@ -14,6 +14,7 @@ class Bil(db.Model):
     namn = db.Column(db.String(80), unique=False, nullable=False)
     modell = db.Column(db.String(20), unique=False, nullable=False)
     year = db.Column(db.Integer, unique=False, nullable=False)
+    color = db.Column(db.String(10), unique=False, nullable=False)
 
 db.create_all()
 
